55f86f3a9375269e46253d6ce1dd7994ca6e107a
neworderofjamie <james.knight@manchester.ac.uk>
2016-02-04 16:14:52 +0000

    Call C++ constructors in sark_pre_main

--- spin1_api/spin1_api.c
+++ spin1_api/spin1_api.c
@@ -1832,8 +1832,16 @@ uint spin1_trigger_user_event(uint arg0, uint arg1)
 *
 * SOURCE
 */
+extern void (*__init_array_start)();
+extern void (*__init_array_end)();
 void sark_pre_main (void)
 {
+  // Loop through any C++ constructors that may be present and call them
+  for (void (**p)() = &__init_array_start; p < &__init_array_end; p++)
+  {
+    (*p)();
+  }
+  
   sark_cpu_state (CPU_STATE_SARK);
   sark_vec->api = 1;
 
