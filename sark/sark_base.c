cc0c337853d3633505fb287b2f66bd64c1e17843
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2016-11-15 16:08:29 +0000

    Added error generators for division by 0

--- sark/sark_base.c
+++ sark/sark_base.c
@@ -825,3 +825,12 @@ void sark_int (void *pc)
 
 
 //------------------------------------------------------------------------------
+
+// Functions to handle division by 0 (simply RTE)
+void __aeabi_idiv0() {
+    rt_error(RTE_DIV0);
+}
+
+void __aeabi_ldiv0() {
+    rt_error(RTE_DIV0);
+}
