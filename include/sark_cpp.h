c05610b43b595d44f74997628b390972556821ba
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-04-27 09:09:48 +0100

    Added required operators when using C++ without stdlibs

--- include/sark_cpp.h
+++ include/sark_cpp.h
@@ -4,3 +4,42 @@ extern "C"
 {
   #include <sark.h>
 }
+#include <cstdlib>
+#include <new>
+
+inline void* operator new(size_t size) noexcept {
+    return sark_alloc(1, size);
+}
+
+inline void operator delete(void *p) noexcept {
+    sark_free(p);
+}
+
+inline void* operator new[](size_t size) noexcept {
+    return operator new(size); // Same as regular new
+}
+
+inline void operator delete[](void *p) noexcept {
+    operator delete(p); // Same as regular delete
+}
+
+inline void* operator new(size_t size, std::nothrow_t) noexcept {
+    return operator new(size); // Same as regular new
+}
+
+inline void operator delete(void *p,  std::nothrow_t) noexcept {
+    operator delete(p); // Same as regular delete
+}
+
+inline void* operator new[](size_t size, std::nothrow_t) noexcept {
+    return operator new(size); // Same as regular new
+}
+
+inline void operator delete[](void *p,  std::nothrow_t) noexcept {
+    operator delete(p); // Same as regular delete
+}
+
+extern "C" inline void __cxa_pure_virtual() {
+    io_printf(IO_BUF, "Pure Virtual Function called");
+    rt_error(RTE_SWERR);
+}
