b562ea3ea3b3d729ef7e7541ba4ed7f21e0d842f
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-11-03 09:22:04 +0000

    Finicky style fix

--- include/sark_cpp.h
+++ include/sark_cpp.h
@@ -7,7 +7,7 @@ extern "C"
 #include <cstdlib>
 #include <new>
 
-inline void* operator new(size_t size) noexcept {
+inline void* operator new (size_t size) noexcept {
     void *p = sark_alloc(1, size);
     while (p == nullptr) {
 	rt_error(RTE_SWERR);
@@ -15,30 +15,30 @@ inline void* operator new(size_t size) noexcept {
     return p;
 }
 
-inline void operator delete(void *p) noexcept {
+inline void operator delete (void *p) noexcept {
     sark_free(p);
 }
 
-inline void* operator new[](size_t size) noexcept {
-    return operator new(size); // Same as regular new
+inline void* operator new [](size_t size) noexcept {
+    return operator new (size); // Same as regular new
 }
 
-inline void operator delete[](void *p) noexcept {
-    operator delete(p); // Same as regular delete
+inline void operator delete [](void *p) noexcept {
+    operator delete (p); // Same as regular delete
 }
 
-inline void* operator new(size_t size, std::nothrow_t) noexcept {
+inline void* operator new (size_t size, std::nothrow_t) noexcept {
     return sark_alloc(1, size);
 }
 
-inline void operator delete(void *p,  std::nothrow_t) noexcept {
-    operator delete(p); // Same as regular delete
+inline void operator delete (void *p,  std::nothrow_t) noexcept {
+    operator delete (p); // Same as regular delete
 }
 
-inline void* operator new[](size_t size, std::nothrow_t) noexcept {
+inline void* operator new [](size_t size, std::nothrow_t) noexcept {
     return sark_alloc(1, size);
 }
 
-inline void operator delete[](void *p,  std::nothrow_t) noexcept {
-    operator delete(p); // Same as regular delete
+inline void operator delete [](void *p,  std::nothrow_t) noexcept {
+    operator delete (p); // Same as regular delete
 }
