00bb0be7f545d5623dd1b37be40e23e096b3e324
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2017-11-16 15:39:17 +0000

    A little more style cleanup

--- apps/pt_demo/drawer.c
+++ apps/pt_demo/drawer.c
@@ -52,10 +52,10 @@ float horizontalFieldOfView = 60.0;
 
 void display(void)
 {
-  glClearColor(1.0, 1.0, 1.0, 0.001);
-  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-  glDrawPixels(windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, viewingFrame);
-  glutSwapBuffers();
+    glClearColor(1.0, 1.0, 1.0, 0.001);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glDrawPixels(windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, viewingFrame);
+    glutSwapBuffers();
 }
 
 
@@ -63,10 +63,10 @@ void reshape(
 	int width,
 	int height)
 {
-  windowWidth = MIN(width, frameWidth);
-  windowHeight = MIN(height, frameHeight);
-  glViewport(0, 0, (GLsizei) width, (GLsizei) height);
-  glLoadIdentity();
+    windowWidth = MIN(width, frameWidth);
+    windowHeight = MIN(height, frameHeight);
+    glViewport(0, 0, (GLsizei) width, (GLsizei) height);
+    glLoadIdentity();
 }
 
 
@@ -75,20 +75,19 @@ void special(
 	int x,
 	int y)
 {
-  switch (key)
-    {
+    switch (key) {
     case GLUT_KEY_UP:
-      turningUpDown = -1;
-      break;
+	turningUpDown = -1;
+	break;
     case GLUT_KEY_DOWN:
-      turningUpDown = 1;
-      break;
+	turningUpDown = 1;
+	break;
     case GLUT_KEY_RIGHT:
-      rolling = -1;
-      break;
+	rolling = -1;
+	break;
     case GLUT_KEY_LEFT:
-      rolling = 1;
-      break;
+	rolling = 1;
+	break;
     }
 }
 
@@ -98,20 +97,19 @@ void specialUp(
 	int x,
 	int y)
 {
-  switch (key)
-    {
+    switch (key) {
     case GLUT_KEY_UP:
-      turningUpDown = 0;
-      break;
+	turningUpDown = 0;
+	break;
     case GLUT_KEY_DOWN:
-      turningUpDown = 0;
-      break;
+	turningUpDown = 0;
+	break;
     case GLUT_KEY_RIGHT:
-      rolling = 0;
-      break;
+	rolling = 0;
+	break;
     case GLUT_KEY_LEFT:
-      rolling = 0;
-      break;
+	rolling = 0;
+	break;
     }
 }
 
@@ -121,26 +119,25 @@ void keyPressed(
 	int x,
 	int y)
 {
-  switch (key)
-    {
+    switch (key) {
     case 'w':
-      moving = 1;
-      break;
+	moving = 1;
+	break;
     case 's':
-      moving = -1;
-      break;
+	moving = -1;
+	break;
     case 'a':
-      turningLeftRight = -1;
-      break;
+	turningLeftRight = -1;
+	break;
     case 'd':
-      turningLeftRight = 1;
-      break;
+	turningLeftRight = 1;
+	break;
     case 'q':
-      strafing = 1;
-      break;
+	strafing = 1;
+	break;
     case 'e':
-      strafing = -1;
-      break;
+	strafing = -1;
+	break;
     }
 }
 
@@ -150,26 +147,25 @@ void keyReleased(
 	int x,
 	int y)
 {
-  switch (key)
-    {
+    switch (key) {
     case 'w':
-      moving = 0;
-      break;
+	moving = 0;
+	break;
     case 's':
-      moving = 0;
-      break;
+	moving = 0;
+	break;
     case 'a':
-      turningLeftRight = 0;
-      break;
+	turningLeftRight = 0;
+	break;
     case 'd':
-      turningLeftRight = 0;
-      break;
+	turningLeftRight = 0;
+	break;
     case 'q':
-      strafing = 0;
-      break;
+	strafing = 0;
+	break;
     case 'e':
-      strafing = 0;
-      break;
+	strafing = 0;
+	break;
     }
 }
 
@@ -177,12 +173,12 @@ void keyReleased(
 Vector3 fVectorNormalise(
 	Vector3 in)
 {
-  float magnitudeReciprocal = 1.0 / sqrt(in.x*in.x + in.y*in.y + in.z*in.z);
-  Vector3 result = {
-	  in.x * magnitudeReciprocal,
-	  in.y * magnitudeReciprocal,
-	  in.z * magnitudeReciprocal};
-  return result;
+    float magnitudeReciprocal = 1.0 / sqrt(in.x*in.x + in.y*in.y + in.z*in.z);
+    Vector3 result = {
+	    in.x * magnitudeReciprocal,
+	    in.y * magnitudeReciprocal,
+	    in.z * magnitudeReciprocal};
+    return result;
 }
 
 
@@ -190,11 +186,11 @@ Vector3 fVectorCrossProduct(
 	Vector3 a,
 	Vector3 b)
 {
-  Vector3 result = {
-	  a.y * b.z - a.z * b.y,
-	  a.z * b.x - a.x * b.z,
-	  a.x * b.y - a.y * b.x};
-  return result;
+    Vector3 result = {
+	    a.y * b.z - a.z * b.y,
+	    a.z * b.x - a.x * b.z,
+	    a.x * b.y - a.y * b.x};
+    return result;
 }
 
 
@@ -205,58 +201,58 @@ Vector3 fVectorRotate(
 	Vector3 rotateAbout,
 	float amount)
 {
-  float c = cos(amount);
-  float s = sin(amount);
-  float t = 1 - cos(amount);
+    float c = cos(amount);
+    float s = sin(amount);
+    float t = 1 - cos(amount);
 
-  Vector3 result;
+    Vector3 result;
 
-  result.x = rotated.x * (t * rotateAbout.x * rotateAbout.x + c) +
-    rotated.y * (t * rotateAbout.x * rotateAbout.y + s * rotateAbout.z) +
-    rotated.z * (t * rotateAbout.x * rotateAbout.z - s * rotateAbout.y);
+    result.x = rotated.x * (t * rotateAbout.x * rotateAbout.x + c) +
+	    rotated.y * (t * rotateAbout.x * rotateAbout.y + s * rotateAbout.z) +
+	    rotated.z * (t * rotateAbout.x * rotateAbout.z - s * rotateAbout.y);
 
-  result.y = rotated.x * (t * rotateAbout.x * rotateAbout.y - s * rotateAbout.z) +
-    rotated.y * (t * rotateAbout.y * rotateAbout.y + c) +
-    rotated.z * (t * rotateAbout.y * rotateAbout.z + s * rotateAbout.x);
+    result.y = rotated.x * (t * rotateAbout.x * rotateAbout.y - s * rotateAbout.z) +
+	    rotated.y * (t * rotateAbout.y * rotateAbout.y + c) +
+	    rotated.z * (t * rotateAbout.y * rotateAbout.z + s * rotateAbout.x);
 
-  result.z = rotated.x * (t * rotateAbout.z * rotateAbout.x + s * rotateAbout.y) +
-    rotated.y * (t * rotateAbout.y * rotateAbout.z - s * rotateAbout.x) +
-    rotated.z * (t * rotateAbout.z * rotateAbout.z + c);
+    result.z = rotated.x * (t * rotateAbout.z * rotateAbout.x + s * rotateAbout.y) +
+	    rotated.y * (t * rotateAbout.y * rotateAbout.z - s * rotateAbout.x) +
+	    rotated.z * (t * rotateAbout.z * rotateAbout.z + c);
 
-  result = fVectorNormalise(result);
-  return result;
+    result = fVectorNormalise(result);
+    return result;
 }
 
 
 void calculateMovement(
 	int timestep)
 {
-  // Forward movement
+    // Forward movement
 
-  position.x += look.x * timestep * moveAmount * moving;
-  position.y += look.y * timestep * moveAmount * moving;
-  position.z += look.z * timestep * moveAmount * moving;
+    position.x += look.x * timestep * moveAmount * moving;
+    position.y += look.y * timestep * moveAmount * moving;
+    position.z += look.z * timestep * moveAmount * moving;
 
-  Vector3 right = fVectorCrossProduct(up, look);
+    Vector3 right = fVectorCrossProduct(up, look);
 
-  // Strafing movement
+    // Strafing movement
 
-  position.x += right.x * timestep * moveAmount * strafing;
-  position.y += right.y * timestep * moveAmount * strafing;
-  position.z += right.z * timestep * moveAmount * strafing;
+    position.x += right.x * timestep * moveAmount * strafing;
+    position.y += right.y * timestep * moveAmount * strafing;
+    position.z += right.z * timestep * moveAmount * strafing;
 
-  // To turn left/right, rotate the look vector around the up vector
+    // To turn left/right, rotate the look vector around the up vector
 
-  look = fVectorRotate(look, up, timestep * turnAmount * turningLeftRight);
+    look = fVectorRotate(look, up, timestep * turnAmount * turningLeftRight);
 
-  // To turn up/down, rotate the look vector and up vector about the right vector
+    // To turn up/down, rotate the look vector and up vector about the right vector
 
-  look = fVectorRotate(look, right, timestep * turnAmount * turningUpDown);
-  up = fVectorRotate(up, right, timestep * turnAmount * turningUpDown);
+    look = fVectorRotate(look, right, timestep * turnAmount * turningUpDown);
+    up = fVectorRotate(up, right, timestep * turnAmount * turningUpDown);
 
-  // To roll, rotate the up vector around the look vector
+    // To roll, rotate the up vector around the look vector
 
-  up = fVectorRotate(up, look, timestep * turnAmount * rolling);
+    up = fVectorRotate(up, look, timestep * turnAmount * rolling);
 }
 
 
@@ -264,19 +260,18 @@ clock_t prevTime;
 
 void idleFunctionLoop(void)
 {
-  //reshape(frameWidth, frameHeight);
+    //reshape(frameWidth, frameHeight);
 
-  clock_t currTime = clock();
+    clock_t currTime = clock();
 
-  // Calculate movement ten times a second
+    // Calculate movement ten times a second
 
-  if (currTime > prevTime + 10000)
-    {
-      int timeSinceLastUpdate = currTime - prevTime;
-      calculateMovement(timeSinceLastUpdate);
+    if (currTime > prevTime + 10000) {
+	int timeSinceLastUpdate = currTime - prevTime;
+	calculateMovement(timeSinceLastUpdate);
 
-      prevTime = currTime;
-      display();
+	prevTime = currTime;
+	display();
     }
 }
 
@@ -287,52 +282,51 @@ int main(
 {
 
 #ifdef WIN32
-  WSADATA wsaData;
-  if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0) {
-      fprintf(stderr, "WSAStartup failed.\n");
-      exit(1);
-  }
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0) {
+	fprintf(stderr, "WSAStartup failed.\n");
+	exit(1);
+    }
 #endif
 
-  pthread_t p1;				// this sets up the thread that can come back to here from type
+    pthread_t p1;			// this sets up the thread that can come back to here from type
 
-  init_udp_server_spinnaker();		//initialization of the port for receiving SpiNNaker frames
+    init_udp_server_spinnaker();	//initialization of the port for receiving SpiNNaker frames
 
-  frameHeight = (argc > 1 ? atoi(argv[1]) : 256);
-  frameWidth = (int) ((((int) horizontalFieldOfView * frameHeight)) / verticalFieldOfView);
+    frameHeight = (argc > 1 ? atoi(argv[1]) : 256);
+    frameWidth = (int) (((int) horizontalFieldOfView * frameHeight) / verticalFieldOfView);
 
-  prevTime = clock();
-  viewingFrame = (unsigned char*) malloc(sizeof(unsigned char) * frameWidth * frameHeight * 3);
+    prevTime = clock();
+    viewingFrame = (unsigned char*) malloc(sizeof(unsigned char) * frameWidth * frameHeight * 3);
 
-  receivedFrame = (unsigned int*) malloc(sizeof(unsigned int) * frameWidth * frameHeight);
+    receivedFrame = (unsigned int*) malloc(sizeof(unsigned int) * frameWidth * frameHeight);
 
-  int i;
-  for (i = 0; i < frameWidth * frameHeight; i++)
-    {
-      receivedFrame[i] = 0;
+    int i;
+    for (i = 0; i < frameWidth * frameHeight; i++) {
+	receivedFrame[i] = 0;
     }
 
-  pthread_create(&p1, NULL, input_thread, NULL); // away it goes
+    pthread_create(&p1, NULL, input_thread, NULL); // away it goes
 
-  glutInit(&argc, argv);		// Initialise OpenGL
-  glutInitDisplayMode(GLUT_DOUBLE);	// Set the display mode
-  glutInitWindowSize(frameWidth, frameHeight); // Set the window size
-  glutInitWindowPosition(0, 0);		// Set the window position
-  glutCreateWindow("Path Tracer");	// Create the window
+    glutInit(&argc, argv);		// Initialise OpenGL
+    glutInitDisplayMode(GLUT_DOUBLE);	// Set the display mode
+    glutInitWindowSize(frameWidth, frameHeight); // Set the window size
+    glutInitWindowPosition(0, 0);	// Set the window position
+    glutCreateWindow("Path Tracer");	// Create the window
 
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
 
-  glutDisplayFunc(display);		// Register the "display" function
-  glutReshapeFunc(reshape);		// Register the "reshape" function
-  glutSpecialFunc(special);
-  glutSpecialUpFunc(specialUp);
-  glutKeyboardFunc(keyPressed);
-  glutKeyboardUpFunc(keyReleased);
-  glutIdleFunc(idleFunctionLoop);
+    glutDisplayFunc(display);		// Register the "display" function
+    glutReshapeFunc(reshape);		// Register the "reshape" function
+    glutSpecialFunc(special);
+    glutSpecialUpFunc(specialUp);
+    glutKeyboardFunc(keyPressed);
+    glutKeyboardUpFunc(keyReleased);
+    glutIdleFunc(idleFunctionLoop);
 
-  glutMainLoop(); // Enter the main OpenGL loop
+    glutMainLoop();			// Enter the main OpenGL loop
 
-  return 0;
+    return 0;
 }
