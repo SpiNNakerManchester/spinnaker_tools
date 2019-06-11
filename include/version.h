96d4c4efc2bee7d91c067293c6b36eecda5ec76f
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2019-06-11 13:46:27 +0100

    I see no reason to add this information to the .h file

--- include/version.h
+++ include/version.h
@@ -14,10 +14,5 @@
 
 #define SLLT_VER_STR    "3.2.4"
 #define SLLT_VER_NUM    0x030204
-#define SLLT_VER_MON    "September"
-#define SLLT_VER_DAY    "25"
-#define SLLT_VER_YEAR   "2017"
-#define SLLT_VER_NAME   "Riptalon"
-
 
 #endif
