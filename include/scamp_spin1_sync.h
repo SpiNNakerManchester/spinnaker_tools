8c57f5720c9f3ab1b989487737b5e4352a27f338
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2020-09-03 14:19:50 +0100

    Add license

--- include/scamp_spin1_sync.h
+++ include/scamp_spin1_sync.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2019-2020 The University of Manchester
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //! Clock drift fixed-point number definition in terms of the number of
 //! fractional bits and the mask to get the fractional bits
 #define DRIFT_FP_BITS 17U
